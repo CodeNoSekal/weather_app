601880fe300561253b2f86f4357974c6b66544ea
Dmitry <lvbnhbq633@gmail.com>
2026-02-09 05:18:25 +0300

    Чёто наворотил

--- app/src/main/java/com/polyhub/weather/MainActivity.kt
+++ app/src/main/java/com/polyhub/weather/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -18,10 +20,17 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.polyhub.weather.ui.theme.WeatherTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,47 +48,95 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Screen() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Row {
-                        Image(
-                            imageVector = Icons.Filled.LocationOn,
-                            contentDescription = "Location"
-                        )
-                        Text("Местоположение")
-                    }
-                },
-                navigationIcon = {
-                    IconButton( onClick = {}) {
-                        Icon(
-                            imageVector = Icons.Filled.Menu,
-                            contentDescription = "Menu"
-                        )
-                    }
-                }
-            )
-        },
+    Box(
         modifier = Modifier
             .fillMaxSize()
-    ) { innerPadding ->
-        Content(
-            modifier = Modifier.padding(innerPadding)
+    ){
+        Image(
+            painter = painterResource(R.drawable.background),
+            contentDescription = "Background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
         )
+
+        Scaffold(
+            containerColor = Color.Transparent,
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Row {
+                            Text("Местоположение")
+                        }
+                    },
+                    navigationIcon = {
+                        IconButton( onClick = {}) {
+                            Icon(
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = "Menu"
+                            )
+                        }
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color.Transparent
+                    )
+                )
+            },
+
+            modifier = Modifier
+                .fillMaxSize()
+        ) { innerPadding ->
+            Content(
+                modifier = Modifier
+                    .padding(innerPadding)
+            )
+        }
     }
+
 }
 
 @Composable
 fun Content(
-    modifier: Modifier
+    modifier: Modifier = Modifier
 ){
-    Text("10")
+    Box(
+        modifier = modifier
+    ){
+        Column() {
+            AppHeader()
+            MainContent()
+        }
+    }
+}
+
+@Composable
+fun AppHeader(){
+
+}
+
+@Composable
+fun MainContent(
+){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Text(
+            text = "20${stringResource(R.string.degree)}",
+            fontSize = 72.sp
+        )
+
+        Text(
+            text = "Переменная облачность"
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun WeatherPreview() {
     WeatherTheme {
         Screen()
     }
